c9bd102a3dd622d25aa9170a2de533915a6361c2
PiDve <univercomput@gmail.com>
2021-05-29 03:10:22 -0300

    ++++

--- sistemas-distribuidos/atividades/atividade2/resolucao/clientUDP.py
+++ sistemas-distribuidos/atividades/atividade2/resolucao/clientUDP.py
@@ -16,18 +16,18 @@ def main(mensagem,cliente):
             clientSocket.send((str(i)+";"+cliente).encode())
             modifiedSentence = clientSocket.recv(1024)
             print("Vindo do servidor", modifiedSentence.decode())
-        
+
     except Exception as e:        
-        print(e)
-    clientSocket.close()
+        print(" ")
+    # clientSocket.close()
 
 if __name__ == '__main__':
     pessoa1 = threading.Thread(target=main, args=["1;4;VVFF,1;4;VVFV,1;5;VVVFV,1;4;FFFV,1;4;VVFV","cicrano"])
     pessoa2 = threading.Thread(target=main, args=["1;4;VFFF,1;4;FFFF,1;5;FVVFF,1;4;VVVV,1;4;VVFV","fulano"])
-    # pessoa3 = threading.Thread(target=main, args=["1;4;VVFV,1;4;VFFF,1;5;FFFFF,1;4;VVFV,1;4;FVFV","fulano de tal"])
+    pessoa3 = threading.Thread(target=main, args=["1;4;VVFV,1;4;VFFF,1;5;FFFFF,1;4;VVFV,1;4;FVFV","fulano de tal"])
     
     pessoa1.start()
     pessoa2.start()
-    # pessoa3.start()
+    pessoa3.start()
 
 
