b7c5cadaf3a44ba004274412e56b175be4e88c47
PiDve <univercomput@gmail.com>
2021-06-08 23:54:00 -0300

    +++

---
+++ SISTEMAS-DISTRIBUIDOS/atividades/trabalhofinal1/resolucao/srv2/srv2.py
@@ -0,0 +1,25 @@
+ 
+import sys, struct
+from socket import *
+serverName = '225.0.0.2'
+serverPort = 9000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+# bind udp port
+clientSocket.bind(('', serverPort))
+
+# set mcast group
+mreq = struct.pack('4sl', inet_aton(serverName), INADDR_ANY)
+clientSocket.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+
+print("running server: "+serverName+" "+str(serverPort))
+
+try:
+    while 1:
+        data, addr = clientSocket.recvfrom(1024)
+        # print(str(len(data))+" bytes from: "+str(data)+" "+addr)
+        print(data)
+except KeyboardInterrupt:
+    print('done')
+    sys.exit(0)
