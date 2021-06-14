78466d733b7df7fe899378448de35a49e46f1edb
PiDve <univercomput@gmail.com>
2021-06-14 12:50:19 -0300

    documentado

---
+++ SISTEMAS-DISTRIBUIDOS/atividades/trabalhofinal1/resolucao/srv2/c2.py
@@ -0,0 +1,183 @@
+# https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/
+
+import sys
+
+# importando tudo da biblioteca sockets
+from socket import *
+
+#scripts extras para data
+from datetime import datetime
+
+
+# definindo nome do servidor para conectar
+serverName = '225.0.0.2'
+
+# definindo porta para comunicação udp
+serverPort = 9000
+
+#configurado socket udp para o cliente do par
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+# importações necessárias para criptografia RSA e geração de chaves
+# públicas e privadas
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+# setando 2 segundos de espera para o socket
+clientSocket.settimeout(2)
+
+# Gerando a chave privada para esse par específico
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+)
+
+# Gerando chave pública a partir da chave privada desse par
+public_key = private_key.public_key()
+
+# Convertendo a chave privada em bytes para salvar como arquivo .pem
+private_bytes_key = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.PKCS8,
+    encryption_algorithm=serialization.BestAvailableEncryption(b'3322'))
+
+# Convertendo a chave pública em bytes para envio 
+public_bytes_key = public_key.public_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+# Salvando chave privada em arquivo
+arq = open('par2_pri_key.pem', 'wb')
+arq.write(private_bytes_key)
+arq.close()
+
+# enviando e avisando sobre chave pública para os outros processos
+data_e_hora_atuais = datetime.now()
+data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+print("sending public key - " + data_e_hora_em_texto)
+clientSocket.sendto("pub_key".encode(), (serverName, serverPort)) # avisando sobre o envio da chave pública
+clientSocket.sendto("par2_pub_key".encode(), (serverName, serverPort)) # enviando o nome do par
+clientSocket.sendto(public_bytes_key, (serverName, serverPort)) # enviando a chave em bytes
+data_e_hora_atuais = datetime.now() 
+data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+print("------------ connection closed public key sent - " +
+      data_e_hora_em_texto)
+
+
+try:
+    # Loop infinito para realizar ping nos pares e solicitar arquivos para baixar
+    while 1:
+
+        # espera entrada para saber qual função vai ativar se é o ping ou file (para baixar arquivos)
+        sentence = input('Digite o nome do comando:')
+
+        # if que realiza o ping no multcast os pares conectados irão retornar pong
+        if (sentence == "ping"):
+            #envio da mensagem ping
+            clientSocket.sendto(sentence.encode(), (serverName, serverPort))
+
+            # while fica recebendo todos os envios de pong dos pares
+            while 1:
+                try:
+                    dados, addr = clientSocket.recvfrom(2048) #recebe os valores
+                    print(dados, addr) #printa na tela o pong recebido
+                except:
+                    break # se tiver alguma exceção ele finaliza o while
+                if not dados:
+                    break # se não tiver recebido mais dados ele também finaliza
+            data_e_hora_atuais = datetime.now()
+            data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+            print("------------ connection closed pong - " + data_e_hora_em_texto) #avisando a finalização do pong
+
+        # if para opção para fazer download de algum arquivo dos pares conectados
+        if (sentence == "file"):
+            clientSocket.sendto(sentence.encode(), (serverName, serverPort)) # envia solicitação para os pares
+
+
+            nomeArquivo = input('Digite o nome do arquivo:') # defini o nome do arquivo
+
+            clientSocket.sendto(nomeArquivo.encode(), (serverName, serverPort)) # envia o nome do arquivo para os pares
+
+            # while infinito que recebe todas as repostas dos pares
+            while 1:
+                try:
+                    dados, addr = clientSocket.recvfrom(2048) # recebendo respostas
+                    print(dados, addr) # imprimindo na tela os dados recebidos
+                except:
+                    break # se der execeção ele para
+                if not dados:
+                    break # se não tiver mais dados ele para o while
+            
+            #escolhendo de qual par receber o arquivo e o seu identificador de par
+            sentence = input(
+                'Confirme de qual endereço (processo) deseja receber o arquivo? (mande id do par escolhido e o seu id de par ex. 2 1)'
+            )
+
+            #Caso queira da exit na escolha do par ou nenhum par tenho arquivo desejado
+            if(sentence == "e"):
+                break
+
+            #enviando escolha do par
+            clientSocket.sendto(sentence.encode(), (serverName, serverPort))
+
+            # abrindo arquivo para salvar os dados recebidos linha por linha 
+            arq = open(nomeArquivo + '.txt', 'w')
+            l = 0 #contador de linhas para o print
+
+            password = b'3322' # senha para ler a chave privada
+
+            #lendo a chave privada
+            with open("par1_pri_key.pem", "rb") as key_file:
+                private_key = serialization.load_pem_private_key(
+                    key_file.read(),
+                    password=password,
+                )
+
+            # while infinito para receber todas as linhas do arquivo que está baixando
+            while 1:
+                try:
+                    dados, addr = clientSocket.recvfrom(2048) #recebimento dos dados
+                    cipher_linha = dados # trocando de variável a linha criptografada
+                    
+                    # descriptografando a linha recebida com a chave privada do par
+                    plaintext = private_key.decrypt(
+                        cipher_linha,
+                        padding.OAEP(
+                            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                            algorithm=hashes.SHA256(),
+                            label=None
+                        )
+                    )
+                    l += 1 # contando linha para o print
+                    print("linha " + str(l) + ": " + plaintext.decode('utf-8'))
+                    
+                    # escrevendo linha no arquivo, uso do decode é por que a linha ainda está em bytes
+                    arq.write(plaintext.decode('utf-8'))
+
+                except Exception as e:
+                    # esse if, é por que quando termina o envio completo de linhas ele dá timed-out 
+                    # porém envia tudo e salva certo no arquivo
+                    if str(e) == 'timed out': 
+                        data_e_hora_atuais = datetime.now()
+                        data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+                        print("------------ connection closed file received - " +
+                        data_e_hora_em_texto)
+                    
+                    #qualquer outro tipo de execeção vai ser erro de envio
+                    else:
+                        print(e)
+                        data_e_hora_atuais = datetime.now()
+                        data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+                        print("------------ connection closed file not received - " +
+                            data_e_hora_em_texto)
+
+                    # fechamento do envio e finalização do while infinito
+                    arq.close()
+                    break
+
+except KeyboardInterrupt:
+    # caso clique control + c ele interrompe a execução do bind e finaliza o script
+    print('done')
+    sys.exit(0)
---
+++ SISTEMAS-DISTRIBUIDOS/atividades/trabalhofinal1/resolucao/srv3/s3.py
@@ -0,0 +1,195 @@
+#importação de bibliotecas para configuração de socket udp multicast
+import sys, struct, socket
+import json
+
+from datetime import datetime
+
+# importação de bibliotecas para geração de chaves públicas e privadas
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+
+# define o nome do server do par
+serverName = '225.0.0.3'
+#define a porta para conexão
+serverPort = 9000
+
+# configurando socket udp
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Allow multiple sockets to use the same PORT number
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# bind udp port
+serverSocket.bind(('', serverPort))
+
+# set mcast group
+mreq = struct.pack('4sl', socket.inet_aton(serverName), socket.INADDR_ANY)
+serverSocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+# imprimindo na tela o início do servidor do par
+data_e_hora_atuais = datetime.now()
+data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
+print("running server: " + serverName + ":" + str(serverPort) + " - " +
+      data_e_hora_em_texto)
+
+nomeArquivoKeyPublic = ""
+
+cont = {"escolhido": 0, "falhas": 0, "sucesso": 0}
+
+# try e except para salvamento ou leitura do arquivo rel.txt que contém o histórico de cada par
+try:
+    with open('rel3.txt', 'r') as key_file:
+        result = key_file.read()
+except:
+    with open('rel3.txt', 'w') as key_file:
+        key_file.write(str(cont))
+
+# try para while infinito que mantém o servidor do par online 
+try:
+    while 1:
+
+        data, addr = serverSocket.recvfrom(4096)
+
+        # opção de recebimento da chave pública
+        if (data.decode() == "pub_key"):
+            # recebe nome para chave publica
+            client, addr = serverSocket.recvfrom(4096)
+            # guarda nome para chave em uma variável
+            nomeArquivoKeyPublic = client.decode()
+            
+            # try para salvar a chave pública
+            try:
+                
+                # abrindo arquivo
+                arq = open(nomeArquivoKeyPublic + '.pem', 'wb')
+                data, addr = serverSocket.recvfrom(4096) # recebendo chave pública em bytes
+
+                arq.write(data) # salvando chave pública em arquivo
+                arq.close() # fechando arquivo
+
+                # imprimindo recebimento de chave pública
+                data_e_hora_atuais = datetime.now()
+                data_e_hora_em_texto = data_e_hora_atuais.strftime(
+                    '%d/%m/%Y %H:%M')
+                print("recebido pub_key - " + nomeArquivoKeyPublic + " - " +
+                      data_e_hora_em_texto)
+
+            except Exception as e:
+                print(e)
+                arq.close()
+
+        #opção de recebimento do ping
+        if (data.decode() == "ping"):
+
+            # imprimindo na tela o print recebido
+            data_e_hora_atuais = datetime.now()
+            data_e_hora_em_texto = data_e_hora_atuais.strftime(
+                '%d/%m/%Y %H:%M')
+            print("ping recebido - " + data_e_hora_em_texto)
+            
+            #enviando de volta o pong para o par que enviou o ping
+            serverSocket.sendto("pong from 225.0.0.3".encode(), ('', addr[1]))
+
+        # opção de recebimento do file
+        if (data.decode() == "file"):
+            try:
+
+                # recebe o nome do arquivo para download
+                data, addr = serverSocket.recvfrom(4096)
+
+                #verifica se o par possui o arquivo 
+                arq = open(data.decode() + '.txt', 'r')
+
+                # se não der exceção o par possui o arquivo
+                # abre o rel3.txt que possui a nota do par para downloads de arquivos 
+                with open('rel3.txt', 'r') as key_file:
+                    result = key_file.read()
+
+                # concatenação de strings para retornar ao par externo que possui o arquivo para download
+                conctResult = "file disponivel from 225.0.0.3 id=3 " + result
+
+                # retorno da resposta
+                serverSocket.sendto(conctResult.encode(), ('', addr[1]))
+
+                # recebe para qual par deve enviar o download
+                data, addr = serverSocket.recvfrom(4096)
+                
+                # separa em list a resposta
+                r = data.decode().split()
+
+                # se o primeiro termo da lista for 3 ele continuar a execução para enviar o arquivo linha-a-linha
+                if (r[0] == "3"):
+                    result = result.replace("'", '"') # arruma a string para converter em dicionário
+                    cont = json.loads(result) # transforma string para dicionário para manipulação
+                    cont["escolhido"] += 1 # conta mais um para escolhido
+                    
+                    # tenta abrir a chave pública do par que solicitou o download,
+                    # pegando o segundo argumento da lista,
+                    # recebida.
+                    try:
+
+                        #realiza leitura da chave pública
+                        with open('par' + r[1] + '_pub_key.pem',
+                                  'rb') as key_file:
+                            public_key = serialization.load_pem_public_key(
+                                key_file.read())
+
+                        # for para envio de linha por linha
+                        for i in arq.readlines():
+
+                            # criptografa cada linha antes de enviar com a chave pública do par que solicitou
+                            cipher_text = public_key.encrypt(
+                                bytes(i, encoding="utf-8"),
+                                padding.OAEP(mgf=padding.MGF1(
+                                    algorithm=hashes.SHA256()),
+                                             algorithm=hashes.SHA256(),
+                                             label=None))
+
+                            #envia a mensagem criptograda
+                            serverSocket.sendto(cipher_text, ('', addr[1]))
+
+                        # após finalizar o for fecha o arquivo aberto
+                        arq.close()
+
+                        # imprimi na tela o envio do arquivo
+                        data_e_hora_atuais = datetime.now()
+                        data_e_hora_em_texto = data_e_hora_atuais.strftime(
+                            '%d/%m/%Y %H:%M')
+                        print("file sent - " + data_e_hora_em_texto)
+
+                        cont["sucesso"] += 1 # conta mais para sucesso de envios
+                        print(cont) # imprimi o contaador
+
+                        #salva o contador no arquivo de relatório novamente
+                        with open('rel3.txt', 'w') as key_file:
+                            key_file.write(str(cont))
+
+                    except Exception as e:
+                        # imprimi na tela o erro e que o arquivo não foi enviado
+                        print(e) 
+                        data_e_hora_atuais = datetime.now()
+                        data_e_hora_em_texto = data_e_hora_atuais.strftime(
+                            '%d/%m/%Y %H:%M')
+                        print("file not sent - " + data_e_hora_em_texto)
+                        
+                        cont["falhas"] += 1 # conta mais um como falha de envio
+                        print(cont) # imprimi o contador
+
+                        #salva o contador no arquivo de relatório novamente
+                        with open('rel3.txt', 'w') as key_file:
+                            key_file.write(str(cont))
+                arq.close()
+
+            except Exception as e:
+                # se o aquivo não existir envia para o par a informação
+                print(e)
+                serverSocket.sendto(
+                    "Arquivo inexistente from 225.0.0.3 ou Falha na conexao".
+                    encode(), ('', addr[1]))
+                
+
+except KeyboardInterrupt:
+    # caso clique control + c ele interrompe a execução do bind e finaliza o script
+    print('done')
+    sys.exit(0)
