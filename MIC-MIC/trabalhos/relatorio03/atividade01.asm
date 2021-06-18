636c2d54b10c2aa178148248333d5921df2698da
PiDve <univercomput@gmail.com>
2021-06-18 15:34:50 -0300

    ===

--- MIC-MIC/trabalhos/relatorio03/atividade01.asm
+++ MIC-MIC/trabalhos/relatorio03/atividade01.asm
@@ -4,9 +4,10 @@ org 100h ; 64Kbytes
 
 .DATA ; 0Dh, 0Ah e 24h mudanca de linha de cursor apos escrita da mensagem
 
-mensageml DB 'Mensagem 1', 0Dh, 0Ah, 24h ; ODh tecla <ENTER>
-mensagem2 DB 'Mensagem 2', 0Dh, 0Ah, 24h ; OAh mudanca de linha
+mensageml DB 'Mensagem 1', 0Dh, 0Ah, 24h ; 0Dh tecla <ENTER>
+mensagem2 DB 'Mensagem 2', 0Dh, 0Ah, 24h ; 0Ah mudanca de linha
 mensagem3 DB 'Mensagem 3', 0Dh, 0Ah, 24h ; 24h identifica o final de uma sequencia de caracter ($)
+
 
 .CODE
 
@@ -31,10 +32,7 @@ saida:
 INT 20h
 
 escreva PROC NEAR ;procedimento escreva
-
 MOV AH, 09h ;apresentacao do caracter 09h
-
 INT 21h; interrupcao 21h
-
 RET ; retorno do procedimento para a 1 linha apos sua chamada (30)
 escreva ENDP
