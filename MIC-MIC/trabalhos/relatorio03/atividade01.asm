d7f32089f770f6f7673395a9ccb212b209f514f8
PiDve <univercomput@gmail.com>
2021-06-18 11:52:34 -0300

    +++

---
+++ MIC-MIC/trabalhos/relatorio03/atividade01.asm
@@ -0,0 +1,40 @@
+;* Programa: MENSAGEMA. ASM *
+
+org 100h ; 64Kbytes
+
+.DATA ; 0Dh, 0Ah e 24h mudanca de linha de cursor apos escrita da mensagem
+
+mensageml DB 'Mensagem 1', 0Dh, 0Ah, 24h ; ODh tecla <ENTER>
+mensagem2 DB 'Mensagem 2', 0Dh, 0Ah, 24h ; OAh mudanca de linha
+mensagem3 DB 'Mensagem 3', 0Dh, 0Ah, 24h ; 24h identifica o final de uma sequencia de caracter ($)
+
+.CODE
+
+JMP salto3 ; salta para a linha 27
+
+saltol:
+LEA DX, mensageml
+CALL escreva
+JMP saida
+
+salto2:
+LEA DX, mensagem2
+CALL escreva
+JMP saltol
+
+salto3:
+LEA DX, mensagem3 ; envio da mensagem 3
+CALL escreva ;chama do procedimento escreva pelo CALL
+JMP salto2
+
+saida:
+INT 20h
+
+escreva PROC NEAR ;procedimento escreva
+
+MOV AH, 09h ;apresentacao do caracter 09h
+
+INT 21h; interrupcao 21h
+
+RET ; retorno do procedimento para a 1 linha apos sua chamada (30)
+escreva ENDP
--- MIC-MIC/trabalhos/relatorio03/pratica01.asm
+++ MIC-MIC/trabalhos/relatorio03/pratica01.asm
@@ -1,48 +1,40 @@
-:* Programa: MENSAGEMA. ASM *
+;* Programa: MENSAGEMA. ASM *
 
-org 100h : 64Kbytes
+org 100h ; 64Kbytes
 
-DATA : ODh, OAh e 24h mudanca de linha de cursor apos escrita da
-mensagem
+.DATA ; 0Dh, 0Ah e 24h mudanca de linha de cursor apos escrita da mensagem
 
-mensageml DB 'Mensagem 1', ODh, OAh, 24h; ODh tecla <ENTER>
-
-mensagem? DB 'Mensagem 2', ODh, OAh, 24h; OAh mudanca de linha
-
-mensagem3 DB 'Mensagem 3', ODh, OAh, 24h ; 24h identifica o final de uma sequencia
-de caracter ($)
+mensageml DB 'Mensagem 1', 0Dh, 0Ah, 24h ; ODh tecla <ENTER>
+mensagem2 DB 'Mensagem 2', 0Dh, 0Ah, 24h ; OAh mudanca de linha
+mensagem3 DB 'Mensagem 3', 0Dh, 0Ah, 24h ; 24h identifica o final de uma sequencia de caracter ($)
 
 .CODE
 
-JMP salto3 : salta para a linha 27
-saltol:
+JMP salto3 ; salta para a linha 27
 
+saltol:
 LEA DX, mensageml
 CALL escreva
-
 JMP saida
 
 salto2:
-
-LEA DX, mensagem?
+LEA DX, mensagem2
 CALL escreva
-
 JMP saltol
 
 salto3:
-
-LEA DX, mensagem3 : envio da mensagem 3
-CALL escreva ;chama do procedimento escreva pelo CALL linha 35
-JMP salto?
+LEA DX, mensagem3 ; envio da mensagem 3
+CALL escreva ;chama do procedimento escreva pelo CALL
+JMP salto2
 
 saida:
 INT 20h
 
-escreva PROC NEAR : procedimento escreva
+escreva PROC NEAR ;procedimento escreva
 
-MOV AH, 09h: apresentacao do caracter 09h
+MOV AH, 09h ;apresentacao do caracter 09h
 
 INT 21h; interrupcao 21h
 
-RET : retorno do procedimento para a 1 linha apos sua chamada (30)
+RET ; retorno do procedimento para a 1 linha apos sua chamada (30)
 escreva ENDP
