f49d5eea3328564b88fda56aefc2d198b53d49a4
PiDve <univercomput@gmail.com>
2021-06-24 02:31:18 -0300

    +++

--- MIC-MIC/trabalhos/relatorio04/atividade01.asm
+++ MIC-MIC/trabalhos/relatorio04/atividade01.asm
@@ -1,57 +1,69 @@
-; Rodar o código 11 no Emu8086
-;*******************************
-;* Programa: CONDIC3.ASM *
-;*******************************
-
-
 org 100h
 
 .DATA
 msg1 DB 'Entre um valor numerico positivo (de 0 ate 9): ', 24h ; 24h caracter
-de fim de string $
-msg2 DB 0Dh, 0Ah, 'Valor impar', 24h ;0Dh tecla <ENTER>
-msg3 DB 0Dh, 0Ah, 'Valor par', 24h ; 0Ah line feed
+;de fim de string $
+msg2 DB 0Dh, 0Ah, 'Entre um valor numerico positivo 2 (de 0 ate 9): ', 24h ;0Dh tecla <ENTER>
+msg3 DB 0Dh, 0Ah, 'Valor soma: ', 24h ; 0Ah line feed
 msg4 DB 0Dh, 0Ah, 'Caractere invalido', 24h
 
 .CODE
-LEA DX, msg1
-CALL escreva
 
+JMP entrada1     
+JMP entrada2
+JMP imprimir
+        
+entrada1:
+LEA DX, msg2
+CALL escreva ; chamando procedimento escreva para imprimir mensagem no terminal
 MOV AH, 01h ; entrada do caracter pelo teclado
 INT 21h ; controle da acao
-
 CMP AL, 30h ; desvio de deteccao de erro, caso o valor < 30h(0d)
-JL erro  ; desvio para linha 40
-
+JL erro  ; desvio para rotina erro
 CMP AL, 39h ; desvio de deteccao de erro, caso o valor >=39h (9h)
-JG erro  ; desvia para linha 40
-
-SUB AL, 30h ;efetua a subtracao do valor 30h para deixar apenas no
-registrador AL o valor numerico
-AND AL, 01h ;01h=00000001b compara com o registrador PF
-JPE par     ;desvio (JPE quando AND for V)
-JPO impar ;(JPO quando AND for F) ambasdesviam a execucao do
-;programa para o trecho de validade
-par:
-LEA DX, msg3
-CALL escreva
-JMP saida
-
-impar:
+JG erro  ; desvia para rotina erro
+SUB AL, 30h ; efetua a subtracao do valor 30h para deixar apenas no
+OR BL, AL ; registrador AL o valor numerico
+            
+entrada2:
 LEA DX, msg2
 CALL escreva
-JMP saida
+MOV AH, 01h ; entrada do caracter pelo teclado
+INT 21h ; controle da acao
+CMP AL, 30h ; desvio de deteccao de erro, caso o valor < 30h(0d)
+JL erro  ; desvio para rotina erro
+CMP AL, 39h ; desvio de deteccao de erro, caso o valor >=39h (9h)
+JG erro  ; desvia para rotina erro
+SUB AL, 30h ; efetua a subtracao do valor 30h para deixar apenas no
+                  
+ADD BL, AL ; somando AL + BL em BL
+
+imprimir:
+LEA DX, msg3 ; copiando endereco de msg para dx
+CALL escreva ; chamando procedimento escreva para imprimir mensagem no terminal
+MOV AH, 02h ; preparando interrupcao para imprimir caractere em dl
+MOV DL, BL  ; movendo soma de BL para DL
+ADD DL, 30h ; asicionando 30h para mostrar o caractere em ASCII
+CALL resultado ; chamando procedimento para imprimir resultado da soma
+JMP saida ; chamando rotina de finalizacao
 
 erro:
 LEA DX, msg4 ; mensagem armazenada na variavel msg4
-CALL escreva
-JMP saida
+CALL escreva ; chamando procedimento escreva para imprimir mensagem no terminal
+JMP saida ; chamando procedimento escreva para imprimir mensagem no terminal
 
 saida:
 INT 20h
 
 escreva PROC NEAR
-MOV AH, 09h
-INT 21h
+MOV AH, 09h ; preparando interrupcao para imprimir mensagem
+INT 21h ; interrupcao que imprimi mensagem na tela
 RET
 escreva ENDP
+
+resultado PROC NEAR
+MOV AH, 02h ; preparando interrupcao para imprimir caractere
+INT 21h ; interrupcao que imprimi caractere na tela
+RET
+resultado ENDP
+
--- MIC-MIC/trabalhos/relatorio04/atividade02.asm
+++ MIC-MIC/trabalhos/relatorio04/atividade02.asm
@@ -1,20 +1,31 @@
-;*******************************
-;* Programa: LACO1.ASM *
-;*******************************
-
-;programa que repete 5x ALO Mundo
 org 100h
 
 .DATA
-msg DB 'Alo Mundo!', 13d, 12o, 24h ; 13d==0Dh , 12o==0Ah e 24h
-(controle da String)
+msg DB 13d, 12o, 24h ; 13d==0Dh , 12o==0Ah e 24h
+;(controle da String)
 
 .CODE
-LEA DX, msg
-MOV CX, 5d ; estabelece em CX o valor decimal (contagem do laco de
-repeticao)
-MOV AH, 09h
-laco:
-    INT 21h
+MOV BL, 30h ; adiciona 0 em ascII para BL
+MOV CX, 9d ; estabelece em CX o valor decimal (contagem do laco de repeticao)
+
+laco:    
+    CALL contador ; chama procedimento contador
+    CALL escreva ; chama procedimento escreva
     LOOP laco ; CX sera decrementado em 1
-INT 20h
+INT 20h ; finaliza o script
+
+
+escreva PROC NEAR
+LEA DX, msg ; msg para quebra de linha no terminal
+MOV AH, 09h ; preparando interrupcao para imprimir mensagem
+INT 21h ; interrupcao que imprimi mensagem na tela
+RET
+escreva ENDP
+
+contador PROC NEAR
+MOV AH, 02h ; prepara interrupcao para imprimir caractere
+INC BL ; incrementa mais um em BL
+LEA DL, BL ; copia enredeco de BL para DL
+INT 21h ; exibicao do caractere no terminal
+RET    
+contador ENDP
