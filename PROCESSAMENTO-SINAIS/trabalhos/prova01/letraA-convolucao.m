6c31c9264b6e9843d95efa8e1bf6d4c3ee132c05
PiDve <univercomput@gmail.com>
2021-07-17 17:14:53 -0300

    +++

--- PROCESSAMENTO-SINAIS/trabalhos/prova01/letraA-convolucao.m
+++ PROCESSAMENTO-SINAIS/trabalhos/prova01/letraA-convolucao.m
@@ -1,6 +1,10 @@
 #calculando apenas para valores positivos de n
-f_n = [0 0 0 0 0 0 0 0 0 2 0 0]
-g_n = [0 0 0 0 3 0 0 0 0 0 0 0]
+#f_n = [0 0 0 0 0 0 0 0 0 2 0 0]
+#g_n = [0 0 0 0 3 0 0 0 0 0 0 0]
+f_n = zeros(0,16)
+g_n = zeros(0,16)
+f_n(10) = 2
+g_n(5) = 3
 
 subplot(311)
 stem(f_n)
@@ -14,8 +18,11 @@ xlabel (sprintf ("n -> 0 à 10"));
 ylabel (sprintf ("valores g[n]"));
 title (sprintf ("gráfico g[n]"));
 x_n = conv(f_n,g_n)
+
+
 subplot(313)
 stem(x_n)
 xlabel (sprintf ("n -> 0 à 10"));
 ylabel (sprintf ("valores x[n]"));
 title (sprintf ("convolução x[n]=(f*g)[n]"));
+
