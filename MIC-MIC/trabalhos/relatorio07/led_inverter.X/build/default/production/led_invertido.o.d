2ba17f566c80c1ea2bda0c5de2590cbb28033f27
PiDve <univercomput@gmail.com>
2021-07-21 22:34:37 -0300

    ===

---
+++ MIC-MIC/trabalhos/relatorio07/led_inverter.X/build/default/production/led_invertido.o.d
@@ -0,0 +1,3 @@
+build/default/production/led_invertido.o:  \
+	C:/Users/Iagof/Documents/Estudos/Per-odo-6-facul/MIC-MIC/trabalhos/relatorio07/led_inverter.X/led_invertido.asm  \
+
---
+++ MIC-MIC/trabalhos/relatorio07/semaforo-21-07.X/build/default/production/semaforo.o.d
@@ -0,0 +1,3 @@
+build/default/production/semaforo.o:  \
+	C:/Users/Iagof/Documents/Estudos/Per-odo-6-facul/MIC-MIC/trabalhos/relatorio07/semaforo-21-07.X/semaforo.asm  \
+
